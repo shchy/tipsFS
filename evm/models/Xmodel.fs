59cc765c83bac07edaba2d44af431c4118aea62d
shch <shch@shchnoMacBook-puro.local>
2018-02-28 15:08:34 +0900

    add paket

--- evm/models/Xmodel.fs
+++ evm/models/Xmodel.fs
@@ -1,73 +1,78 @@
 namespace EVM
 
-// module Model =
-    open System
+open System
 
-    type User =
-        {
-            ID : int
-            Name : string
-        }
+type User =
+    {
+        ID : int
+        Name : string
+    }
 
-    type Tag = string
-
-
-    type TaskItem =
-        {
-            ID : int
-            Name : string
-            Value : double
-        }
-
-    type Range<'T> = 
-        {
-            Start : 'T
-            End : 'T
-        }
-
-    type Sprint =
-        {
-            ID : int
-            Name : string
-            Tasks : TaskItem list
-            Range : Range<DateTime> /// todo 逆にスプリント内の営業日が変化するのを受け入れるほうがわかりやすい？
-        }
-
-    type Project =
-        {
-            ID : int
-            Name : string
-            Sprints : Sprint list
-            Tasks : TaskItem list   /// todo 優先度の概念がある
-            Users : User list
-        }
+type Tag = string
 
 
-    /// ---------こっからEVM色が強いよ
+type TaskItem =
+    {
+        ID : int
+        Name : string
+        Value : double
+    }
+
+type Range<'T> = 
+    {
+        Start : 'T
+        End : 'T
+    }
+
+type Sprint =
+    {
+        ID : int
+        Name : string
+        Tasks : TaskItem list
+        Range : Range<DateTime> /// todo 逆にスプリント内の営業日が変化するのを受け入れるほうがわかりやすい？
+    }
+
+type Project =
+    {
+        ID : int
+        Name : string
+        Sprints : Sprint list
+        Tasks : TaskItem list   /// todo 優先度の概念がある
+        Users : User list
+    }
+
+/// タスクの割当
+type TaskAssign =
+    {
+        TaskID : int
+        UserID : int
+    }
+
+/// ---------こっからEVM色が強いよ
+
+/// 勤怠入力のための登録番号的な位置づけ
+type WorkUnit =
+    {
+        ID : int
+        Name : string
+        ProjectID : int
+        Range : Range<DateTime>
+    }
 
 
-
-    /// TaskItemの進捗履歴
-    type TaskRecord =
-        {
-            TaskID : int
-            Value : double
-            Timestamp : DateTime
-            User : User
-        }
-
+/// TaskItemの進捗履歴
+type TaskRecord =
+    {
+        TaskID : int
+        Value : double
+        Timestamp : DateTime
+        User : User
+    }
 
 
-    /// タスクの割当
-    type TaskAssign =
-        {
-            TaskID : int
-            UserID : int
-        }
-
-    type WorkRecord =
-        {
-            TimeStamp : DateTime
-            User : User
-            Value : double
-        }
+type WorkRecord =
+    {
+        TimeStamp : DateTime
+        User : User
+        Value : double
+    }
