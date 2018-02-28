de021178775d1cbc1417d0f343bc4f5847eac57f
shch <shch@shchnoMacBook-puro.local>
2018-02-28 16:04:59 +0900

    add giraffe

--- evm/Program.fs
+++ evm/Program.fs
@@ -2,13 +2,203 @@
 
 open EVM
 open System
+open System.Security.Claims
+open System.Threading
+open Microsoft.AspNetCore
+open Microsoft.AspNetCore.Builder
+open Microsoft.AspNetCore.Hosting
+open Microsoft.AspNetCore.Http
+open Microsoft.AspNetCore.Http.Features
+open Microsoft.AspNetCore.Authentication
+open Microsoft.AspNetCore.Authentication.Cookies
+open Microsoft.Extensions.Configuration
+open Microsoft.Extensions.Logging
+open Microsoft.Extensions.DependencyInjection
+open FSharp.Control.Tasks.ContextInsensitive
+open Giraffe
+open Giraffe.GiraffeViewEngine
+
+[<CLIMutable>]
+type Person =
+    {
+        Name : string
+    }
+
+let layout (content: XmlNode list) =
+    html [] [
+        head [] [
+            title []  [ encodedText "Giraffe" ]
+        ]
+        body [] content
+    ]
+
+let partial () =
+    p [] [ encodedText "Some partial text." ]
+
+let personView (model : Person) =
+    [
+        div [_class "container"] [
+                h3 [_title "Some title attribute"] [ sprintf "Hello, %s" model.Name |> encodedText ]
+                a [_href "https://github.com/giraffe-fsharp/Giraffe"] [encodedText "Github"]
+            ]
+        div [] [partial()]
+    ] |> layout
+
+// ---------------------------------
+// Error handler
+// ---------------------------------
+
+let errorHandler (ex : Exception) (logger : ILogger) =
+    logger.LogError(EventId(), ex, "An unhandled exception has occurred while executing the request.")
+    clearResponse >=> setStatusCode 500 >=> text ex.Message
+
+// ---------------------------------
+// Web app
+// ---------------------------------
+
+let authScheme = CookieAuthenticationDefaults.AuthenticationScheme
+
+let accessDenied = setStatusCode 401 >=> text "Access Denied"
+
+let mustBeUser = requiresAuthentication accessDenied
+
+let mustBeAdmin =
+    requiresAuthentication accessDenied
+    >=> requiresRole "Admin" accessDenied
+
+let mustBeJohn =
+    requiresAuthentication accessDenied
+    >=> requiresAuthPolicy (fun u -> u.HasClaim (ClaimTypes.Name, "John")) accessDenied
+
+let loginHandler =
+    fun (next : HttpFunc) (ctx : HttpContext) ->
+        task {
+            let issuer = "http://localhost:5000"
+            let claims =
+                [
+                    Claim(ClaimTypes.Name,      "John",  ClaimValueTypes.String, issuer)
+                    Claim(ClaimTypes.Surname,   "Doe",   ClaimValueTypes.String, issuer)
+                    Claim(ClaimTypes.Role,      "Admin", ClaimValueTypes.String, issuer)
+                ]
+            let identity = ClaimsIdentity(claims, authScheme)
+            let user     = ClaimsPrincipal(identity)
+
+            do! ctx.SignInAsync(authScheme, user)
+
+            return! text "Successfully logged in" next ctx
+        }
+
+let userHandler =
+    fun (next : HttpFunc) (ctx : HttpContext) ->
+        text ctx.User.Identity.Name next ctx
+
+let showUserHandler id =
+    mustBeAdmin >=>
+    text (sprintf "User ID: %i" id)
+
+let configuredHandler =
+    fun (next : HttpFunc) (ctx : HttpContext) ->
+        let configuration = ctx.GetService<IConfiguration>()
+        text configuration.["HelloMessage"] next ctx
+
+let fileUploadHandler =
+    fun (next : HttpFunc) (ctx : HttpContext) ->
+        task {
+            return!
+                (match ctx.Request.HasFormContentType with
+                | false -> RequestErrors.BAD_REQUEST "Bad request"
+                | true  ->
+                    ctx.Request.Form.Files
+                    |> Seq.fold (fun acc file -> sprintf "%s\n%s" acc file.FileName) ""
+                    |> text) next ctx
+        }
+
+let fileUploadHandler2 =
+    fun (next : HttpFunc) (ctx : HttpContext) ->
+        task {
+            let formFeature = ctx.Features.Get<IFormFeature>()
+            let! form = formFeature.ReadFormAsync CancellationToken.None
+            return!
+                (form.Files
+                |> Seq.fold (fun acc file -> sprintf "%s\n%s" acc file.FileName) ""
+                |> text) next ctx
+        }
+
+let time() = System.DateTime.Now.ToString()
+
+[<CLIMutable>]
+type Car =
+    {
+        Name   : string
+        Make   : string
+        Wheels : int
+        Built  : DateTime
+    }
+    interface IModelValidation<Car> with
+        member this.Validate() =
+            if this.Wheels > 1 && this.Wheels <= 6 then Ok this
+            else Error (RequestErrors.BAD_REQUEST "Wheels must be a value between 2 and 6.")
+
+let parsingErrorHandler err = RequestErrors.BAD_REQUEST err
+
+let webApp =
+    choose [
+        GET >=>
+            choose [
+                route  "/"           >=> text "index"
+                route  "/ping"       >=> text "pong"
+                route  "/error"      >=> (fun _ _ -> failwith "Something went wrong!")
+                route  "/login"      >=> loginHandler
+                route  "/logout"     >=> signOut authScheme >=> text "Successfully logged out."
+                route  "/user"       >=> mustBeUser >=> userHandler
+                route  "/john-only"  >=> mustBeJohn >=> userHandler
+                routef "/user/%i"    showUserHandler
+                route  "/person"     >=> (personView { Name = "Html Node" } |> htmlView)
+                route  "/once"       >=> (time() |> text)
+                route  "/everytime"  >=> warbler (fun _ -> (time() |> text))
+                route  "/configured" >=> configuredHandler
+                route  "/upload"     >=> fileUploadHandler
+                route  "/upload2"    >=> fileUploadHandler2
+            ]
+        route "/car"  >=> bindModel<Car> None json
+        route "/car2" >=> tryBindQuery<Car> parsingErrorHandler None (validateModel xml)
+        RequestErrors.notFound (text "Not Found") ]
+
+// ---------------------------------
+// Main
+// ---------------------------------
+
+let cookieAuth (o : CookieAuthenticationOptions) =
+    do
+        o.Cookie.HttpOnly     <- true
+        o.Cookie.SecurePolicy <- CookieSecurePolicy.SameAsRequest
+        o.SlidingExpiration   <- true
+        o.ExpireTimeSpan      <- TimeSpan.FromDays 7.0
+
+let configureApp (app : IApplicationBuilder) =
+    app.UseGiraffeErrorHandler(errorHandler)
+       .UseStaticFiles()
+       .UseAuthentication()
+       .UseGiraffe webApp
+
+let configureServices (services : IServiceCollection) =
+    services
+        .AddGiraffe()
+        .AddAuthentication(authScheme)
+        .AddCookie(cookieAuth)   |> ignore
+    services.AddDataProtection() |> ignore
+
+let configureLogging (loggerBuilder : ILoggingBuilder) =
+    loggerBuilder.AddFilter(fun lvl -> lvl.Equals LogLevel.Error)
+                 .AddConsole()
+                 .AddDebug() |> ignore
 
 [<EntryPoint>]
 let main _ =
-
-    
-
-
-
-    
+    WebHost.CreateDefaultBuilder()
+        .Configure(Action<IApplicationBuilder> configureApp)
+        .ConfigureServices(configureServices)
+        .ConfigureLogging(configureLogging)
+        .Build()
+        .Run()
     0
